d330d4250cc559e03e29887da24bba5114cb7f3d
Ieuan <morndas@gmail.com>
2017-11-09 09:09:15 +0000

    added Day 2 Code + tests

---
+++ Advent2016.Test/Day2Tests.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/*
+1 2 3
+4 5 6
+7 8 9
+Suppose your instructions are:
+
+ULL
+RRDDD
+LURDL
+UUUUD
+You start at "5" and move up (to "2"), left (to "1"), and left (you can't, and stay on "1"), so the first button is 1.
+Starting from the previous button ("1"), you move right twice (to "3") and then down three times (stopping at "9" after two moves and ignoring the third), ending up with 9.
+Continuing from "9", you move left, up, right, down, and left, ending with 8.
+Finally, you move up four times (stopping at "2"), then down once, ending with 5.
+So, in this example, the bathroom code is 1985.
+*/
+
+
+namespace Advent2016.Test
+{
+	[TestClass]
+	public class Day2Tests
+	{
+		string answerString = "RLRDDRLLDLRLUDDULLDRUUULDDLRLUDDDLDRRDUDDDLLURDDDLDDDRDURUDRDRRULUUDUDDRRRLRRRRRLRULRLLRULDRUUDRLRRURDDRLRULDLDULLLRULURRUULLRLLDDDDLLDURRUDLDLURDRDRDLUUUDDRDUUDDULLUURRDRLDDULURRRUDLLULULDLLURURUDRRRRUDRLRDLRRLDDRDDLULDLLLURURDUDRRRRUULURLRDULDRLUDRRUDDUULDURUDLDDURRRDLULLUUDRLLDUUDLDRUDDRLLLLLLDUDUDDLRDLRRDRUDDRRRLLRRDLLRLDDURUURRRDDLDUULLDLDLRURDLLLDDRUUDRUDDDDULRLLDUULRUULLLULURRRLLULDLDUDLDLURUDUDULLDLLUUDRRDRLUURURURURDLURUUDLDRLUDDUUDULDULULLLDLDDULLULLDULRRDRULLURRRULLDDDULULURLRDURLLURUDDULLRUDLRURURRDRDUULDRUUDURDURDDLRDUUULDUUDRDURURDRRRURLLDDLLLURURULULUDLRDLDRDRURLRLULRDLU,UDLDURRULDRDDLDUULUDLDUULUURDDRUDRURRRUDRURLLDDRURLDLRDUUURDLLULURDDUDDDRRRURLLDLDLULRDULRLULDLUUDLLRLDLRUUULDDUURDLDDRRDLURLDUDDRURDRRURDURRRLUULURDDLRDLDRRRLDUDRLRLLRLDDUULDURUUULLLRRRRRRRDRRRDRLUULDLDDLULDRDUDLLUDRRUDRUUDULRLUURDDDDRRUUDLURULLLURDULUURDRDDURULRUDRRDLRDUUUUUDDDRDRDDRUDRDDDRLRUUDRDRDDDLUDRDRLDRDDRULURDRLDRUDUDRUULRLLUDRDRLLLLDUDRRLLURDLLLDRRUDDUDRLRLDUDRLURRUUULURDDRUURRLDRLRRRUUDLULDDDRDLDUUURLLUULDDRRUDLDDRUDUDUURURDDRDULLLLLULRRRDLRRRDDDLURDDDDLUULLLRDDURRRRLURRLDDLRUULULRDRDDDDLDUUUUUUDRRULUUUDD,UURDRRUDLURRDDDLUDLRDURUDURDLLLLRDLRLRDDRDRDUUULRDLLDLULULRDUDDRRUUDURULDLUDLRDRUDLDDULLLDDRDLLDULLLURLLRDDLDRDULRRDDULRDURLLRUDRLRRLUDURLDRDLDLRLLLURLRRURDLDURDLUDULRDULLLDRDDRDLDRDULUULURDRRRLDRRUULULLDDRRLDLRUURLRUURLURRLLULUUULRLLDDUDDLRLDUURURUDLRDLURRLLURUDLDLLUDDUULUUUDDDURDLRRDDDLDRUDRLRURUUDULDDLUUDDULLDDRRDDRRRUDUDUDLDLURLDRDLLLLDURDURLRLLLUUDLRRRRUDUDDLDLRUURRLRRLUURRLUDUDRRRRRRRLDUDDRUDDLUDLRDDDRLDUULDRDRRDLDRURDLDRULRLRLUDRDLRRUURUUUUDLDUUULLLRRRRRDLRRURDDLLLLUULDLLRULLUDLLDLLUDLRLRRLRURDDRRL,URDRDLLRDDDLLLDDLURLRURUURRRLUURURDURRLLUDURRLRLDLUURDLULRRDRUDDLULDLDRLDLRLRRLLLDDDUDDDLRURURRLLDRRRURUDLRDDLLDULDDLDRLUUUDRRRULDUULRDDDLRRLLURDDURLULRDUDURRLLDLLRLDUDDRRDDLRLLLDUDRLUURRLLDULRLDLUUUUUDULUDLULUDDUURRURLDLDRRLDLRRUDUDRRDLDUDDLULLDLLRDRURDRDRRLDDDDRDDRLLDDDLLUDRURLURDRRRRRUDDDUDUDDRDUUDRRUDUDRLULDDURULUURUUUURDRULRLRULLDDRRRUULRRRRURUDLDLRDLLDRLURLRUULLURDUDULRRURLRLLRRLLLURULRRRLDDUULLUUULRRDRULUUUUDRDRRDLRURLRLLRLRRRDRDRLDLUURUURULLDLULRRLRRDRULRRLLLDDURULLDLDLDLUUURDLDLUUDULRLLUDDRRDLLDLDLDURLUURRDDRRURDRLUDRLUUUDLDULDLUDRLDUDDLLRUDULLLLLDRRLLUULLUUURRDDUURDLLRDDLRLLU,LDUDRRDLUUDDRLLUUULURLDUDLUDLRLDRURLULRLLDDLRRUUUDDDDRDULDDUUDLRUULDRULLRDRUDDURLDUUURRUDUDRDRDURRDLURRRDRLDLRRRLLLRLURUURRDLLRDLDDLLRDUDDRDUULRULRRURLUDDUDDDUULLUURDULDULLLLRUUUDDRRRLDDDLDLRRDRDRDLUULRLULDRULDLRDRRUDULUDLLUDUULRDLRRUUDDLLDUDDRULURRLULDLDRRULDDRUUDDLURDLRDRLULRRLURRULDUURDLUDLLDRLDULLULDLLRDRDLLLUDLRULLRLDRDDDLDDDLRULDLULLRUUURRLLDUURRLRLDUUULDUURDURRULULRUUURULLLRULLURDDLDRLLRDULLUDLDRRRLLLLDUULRRLDURDURDULULDUURLDUDRLRURRDLUUULURRUDRUUUDRUR";
+
+		[TestMethod]
+		public void DetermineNextKeyFromKnownStart1()
+		{
+			//Start on 5, "ULL"
+			string codeKey = "";
+			string instructions = "ULL";
+
+			var keypad = new KeypadController();
+			codeKey = keypad.DetermineNextKeyFromInstruction(instructions);
+
+			Assert.AreEqual("1", codeKey);
+		}
+
+		[TestMethod]
+		public void DetermineNextKeyFromKnownStart2()
+		{
+			//Start on 1, RRDDD
+			string codeKey = "";
+			string instructions = "RRDDD";
+
+			var keypad = new KeypadController(0,0);
+			codeKey = keypad.DetermineNextKeyFromInstruction(instructions);
+
+			Assert.AreEqual("9", codeKey);
+		}
+
+		[TestMethod]
+		public void DetermineNextKeyFromKnownStart3()
+		{
+			//Start on 9, LURDL
+			string codeKey = "";
+			string instructions = "LURDL";
+
+			var keypad = new KeypadController(2,2);
+			codeKey = keypad.DetermineNextKeyFromInstruction(instructions);
+
+			Assert.AreEqual("8", codeKey);
+		}
+
+		[TestMethod]
+		public void DetermineNextKeyFromKnownStart4()
+		{
+			//Start on 8, UUUUD
+			string codeKey = "";
+			string instructions = "UUUUD";
+
+			var keypad = new KeypadController(2,1);
+			codeKey = keypad.DetermineNextKeyFromInstruction(instructions);
+
+			Assert.AreEqual("5", codeKey);
+
+		}
+
+		[TestMethod]
+		public void DetermineFullCodeInAC()
+		{
+			//Start on 5, ULL, RRDDD, LURDL, UUUUD
+			string codeResponse = "";
+			string instructions = "ULL,RRDDD,LURDL,UUUUD";
+
+			var keypad = new KeypadController();
+
+			codeResponse = keypad.DetermineCodeFromInstructionSet(instructions, ',');
+
+			Assert.AreEqual("1985", codeResponse);
+		}
+
+		[TestMethod]
+		public void FindAnswerStep1()
+		{
+			//Start on 5, ULL, RRDDD, LURDL, UUUUD
+			string codeResponse = "";
+			string instructions = answerString;
+
+			var keypad = new KeypadController();
+
+			codeResponse = keypad.DetermineCodeFromInstructionSet(instructions, ',');
+
+			Assert.AreEqual("18843", codeResponse);
+		}
+
+		[TestMethod]
+		public void HexagonalNextKey1()
+		{
+			//Start on 5, "ULL"
+			string codeKey = "";
+			string instructions = "ULL";
+
+			var keypad = new KeypadController(2,0,KeypadType.Hexagonal);
+			codeKey = keypad.DetermineNextKeyFromInstruction(instructions);
+
+			Assert.AreEqual("5", codeKey);
+		}
+
+		[TestMethod]
+		public void HexagonalNextKey2()
+		{
+			//Start on 5, RRDDD
+			string codeKey = "";
+			string instructions = "RRDDD";
+
+			var keypad = new KeypadController(2,0, KeypadType.Hexagonal);
+			codeKey = keypad.DetermineNextKeyFromInstruction(instructions);
+
+			Assert.AreEqual("D", codeKey);
+		}
+
+		[TestMethod]
+		public void HexagonalNextKey3()
+		{
+			//Start on D, LURDL
+			string codeKey = "";
+			string instructions = "LURDL";
+
+			var keypad = new KeypadController(4,2, KeypadType.Hexagonal);
+			codeKey = keypad.DetermineNextKeyFromInstruction(instructions);
+
+			Assert.AreEqual("B", codeKey);
+		}
+
+		[TestMethod]
+		public void HexagonalNextKey4()
+		{
+			//Start on B, UUUUD
+			string codeKey = "";
+			string instructions = "UUUUD";
+
+			var keypad = new KeypadController(3,2, KeypadType.Hexagonal);
+			codeKey = keypad.DetermineNextKeyFromInstruction(instructions);
+
+			Assert.AreEqual("3", codeKey);
+		}
+
+		[TestMethod]
+		public void GetHexagonalAnswer()
+		{
+			string codeResponse = "";
+			string instructions = answerString;
+
+			var keypad = new KeypadController(2,0,KeypadType.Hexagonal);
+
+			codeResponse = keypad.DetermineCodeFromInstructionSet(instructions, ',');
+
+			Assert.AreEqual("67BB9", codeResponse);
+		}
+	}
+}
---
+++ Advent2016/Keypad.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2016
+{
+	public class KeypadController
+	{
+		private IKeypad keypad;
+
+		public KeypadController(int xStart, int yStart, KeypadType type = KeypadType.Decimal)
+		{
+			switch(type)
+			{
+				case KeypadType.Decimal:
+					xStart = (xStart == -1) ? 1 : xStart;
+					yStart = (yStart == -1) ? 1 : yStart;
+					keypad = new DecimalKeypad(xStart, yStart);
+					break;
+				case KeypadType.Hexagonal:
+					xStart = (xStart == -1) ? 2 : xStart;
+					yStart = (yStart == -1) ? 0 : yStart;
+					keypad = new HexagonalKeypad(xStart, yStart);
+					break;
+				default:
+					break;
+			}
+		}
+
+		public KeypadController() : this(-1, -1, KeypadType.Decimal)
+		{ }
+
+		/// <summary>
+		/// Applies the passed set of instructions and determines the key that it finishes on
+		/// </summary>
+		/// <param name="instructions"></param>
+		/// <returns></returns>
+		public string DetermineNextKeyFromInstruction(string instructions)
+		{
+			foreach (var step in instructions)
+			{
+				keypad.MakeStep(step);
+			}
+
+			return keypad.GetCurrentKey();
+		}
+
+		public string DetermineCodeFromInstructionSet(string instructionSet, char separator)
+		{
+			string code = "";
+
+			List<string> instructions = new List<string>(instructionSet.Split(separator));
+
+			foreach (var instruction in instructions)
+			{
+				var newKey = DetermineNextKeyFromInstruction(instruction);
+
+				code += newKey;
+			}
+
+			return code;
+		}
+	}
+
+
+	interface IKeypad
+	{
+		string GetCurrentKey();
+		void MakeStep(char step);
+	}
+
+	public enum KeypadType
+	{
+		Decimal = 0,
+		Hexagonal
+	}
+
+	public class DecimalKeypad : IKeypad
+	{
+		private string[,] keypad = new string[3,3];
+		private int xPad;
+		private int yPad;
+
+		public DecimalKeypad(int xStart, int yStart)
+		{
+			CreateKeypad();
+			xPad = xStart;
+			yPad = yStart;
+		}
+
+		private void CreateKeypad()
+		{
+			keypad[0, 0] = "1";
+			keypad[0, 1] = "2";
+			keypad[0, 2] = "3";
+			keypad[1, 0] = "4";
+			keypad[1, 1] = "5";
+			keypad[1, 2] = "6";
+			keypad[2, 0] = "7";
+			keypad[2, 1] = "8";
+			keypad[2, 2] = "9";
+		}
+
+		public string GetCurrentKey()
+		{
+			return keypad[xPad, yPad];
+		}
+
+		public void MakeStep(char step)
+		{
+			switch (step)
+			{
+				case 'U':
+					if (xPad > 0)
+					{
+						xPad--;
+					}
+					break;
+				case 'D':
+					if (xPad < (keypad.GetLength(0) - 1))
+					{
+						xPad++;
+					}
+					break;
+				case 'L':
+					if (yPad > 0)
+					{
+						yPad--;
+					}
+					break;
+				case 'R':
+					if (yPad < (keypad.GetLength(1) - 1))
+					{
+						yPad++;
+					}
+					break;
+				default:
+					//Unknown Key!!
+					break;
+			}
+		}
+	}
+
+	public class HexagonalKeypad : IKeypad
+	{
+		private string[,] keypad = new string[5,5];
+		private int xPad;
+		private int yPad;
+
+		public HexagonalKeypad(int xStart, int yStart)
+		{
+			CreateKeypad();
+			xPad = xStart;
+			yPad = yStart;
+		}
+
+		private void CreateKeypad()
+		{
+			/*
+			X X 1 X X
+			X 2 3 4 X
+			5 6 7 8 9
+			X A B C X
+			X X	D X X
+			*/
+
+			keypad[0, 2] = "1";
+			keypad[1, 1] = "2";
+			keypad[1, 2] = "3";
+			keypad[1, 3] = "4";
+			keypad[2, 0] = "5";
+			keypad[2, 1] = "6";
+			keypad[2, 2] = "7";
+			keypad[2, 3] = "8";
+			keypad[2, 4] = "9";
+			keypad[3, 1] = "A";
+			keypad[3, 2] = "B";
+			keypad[3, 3] = "C";
+			keypad[4, 2] = "D";
+		}
+
+		public string GetCurrentKey()
+		{
+			return keypad[xPad, yPad];
+		}
+
+		public void MakeStep(char step)
+		{
+			switch (step)
+			{
+				case 'U':
+					if (xPad > 0 && keypad[xPad - 1, yPad] != null)
+					{
+						xPad--;
+					}
+					break;
+				case 'D':
+					if (xPad < (keypad.GetLength(0) - 1) && keypad[xPad + 1, yPad] != null)
+					{
+						xPad++;
+					}
+					break;
+				case 'L':
+					if (yPad > 0 && keypad[xPad, yPad -1] != null)
+					{
+						yPad--;
+					}
+					break;
+				case 'R':
+					if (yPad < (keypad.GetLength(1) - 1) && keypad[xPad, yPad + 1] != null)
+					{
+						yPad++;
+					}
+					break;
+				default:
+					//Unknown Key!!
+					break;
+			}
+		}
+	}
+}
