2eafee1fd0f249cf851ecf55283422b64bd8bba9
EUTIGPLC\StanleyI <stanleyi@uk.innovation-group.com>
2017-11-09 17:57:45 +0000

    Day3

---
+++ Advent2016.Test/Day3Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Advent2016.Test
+{
+    [TestClass]
+    public class Day3Tests
+    {
+        [TestMethod]
+        public void IsNotValidTriangle()
+        {
+            bool isTriangle = true;
+            int[] sides = { 5, 10, 25 };
+
+            var checker = new TriangleChecker();
+
+            isTriangle = checker.isValidTriangle(sides);
+
+            Assert.IsFalse(isTriangle);
+        }
+
+        [TestMethod]
+        public void IsValidTriangle()
+        {
+            bool isTriangle = false;
+            int[] sides = { 5, 9, 13 };
+
+            var checker = new TriangleChecker();
+
+            isTriangle = checker.isValidTriangle(sides);
+
+            Assert.IsTrue(isTriangle);
+        }
+
+        [TestMethod]
+        public void OnlyOneValidTriangleInList()
+        {
+            IEnumerable<string> list = new string[] {"5 9 13", "5 7 25", "1 2 3"} ;
+
+            var checker = new TriangleChecker();
+
+            var count = checker.GetValidTriangleCountFromList(list);
+
+            Assert.AreEqual(1, count);
+        }
+
+
+        [TestMethod]
+        public void GetInputAnswer()
+        {
+            IEnumerable<string> triangles = System.IO.File.ReadAllLines("../../../TriangleList.txt");
+
+            var checker = new TriangleChecker();
+
+            var count = checker.GetValidTriangleCountFromList(triangles);
+
+            Assert.AreEqual(1921, count);
+            
+        }
+    }
+}
---
+++ Advent2016/TriangleChecker.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Advent2016
+{
+    public class TriangleChecker
+    {
+        public bool isValidTriangle(int[] sideSizes)
+        {
+            bool isValidTriangle = true;
+
+            if (sideSizes.Length > 3)
+            {
+                Console.WriteLine("That's not a Triangle: Too many sides!");
+                isValidTriangle = false;
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (sideSizes[i] >= sideSizes[(i + 1) % 3] + sideSizes[(i + 2) % 3])
+                    {
+                        isValidTriangle = false;
+                    }
+                }
+            }
+
+            return isValidTriangle;
+        }
+
+        public int GetValidTriangleCountFromList(IEnumerable<string> triangleList)
+        {
+            int validTriangleCount = 0;
+            int rowCount = 0;
+            List<List<int>> sideRows = new List<List<int>>();
+
+            foreach ( var triangleString in triangleList)
+            {
+                RegexOptions options = RegexOptions.None;
+                Regex regex = new Regex("[ ]{2,}", options);
+
+                //Split up the input into strings to parse
+                var splitStrings = regex.Replace(triangleString.TrimStart(), " ").Split(' ');
+                List<int> sides = new List<int>();
+
+                //Split up the strings and parts into an int Array
+                foreach(var sidestring in splitStrings)
+                {
+                    sides.Add(int.Parse(sidestring));
+                }
+
+                sideRows.Add(sides);
+                rowCount++;
+
+                if (rowCount == 3)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        int[] triangleArray = { sideRows[0][i], sideRows[1][i], sideRows[2][i] };
+
+                        var thisTriangleIsValid = isValidTriangle(triangleArray);
+
+                        if (thisTriangleIsValid)
+                        {
+                            validTriangleCount++;
+                        }
+                    }
+
+                    rowCount = 0;
+                    sideRows.Clear();
+                }
+            }
+
+            return validTriangleCount;
+        }
+    }
+}
