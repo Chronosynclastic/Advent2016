ea9c77ede921f8d1a830564c36201e0151da026a
EUTIGPLC\StanleyI <stanleyi@uk.innovation-group.com>
2017-11-15 22:03:40 +0000

    Day 4 Code and Tests

---
+++ Advent2016.Test/Day4Tests.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Advent2016.Test
+{
+    /*  aaaaa-bbb-z-y-x-123[abxyz] is a real room because the most common letters are a (5), b (3), and then a tie between x, y, and z, which are listed alphabetically.
+        a-b-c-d-e-f-g-h-987[abcde] is a real room because although the letters are all tied (1 of each), the first five are listed alphabetically.
+        not-a-real-room-404[oarel] is a real room.
+        totally-real-room-200[decoy] is not.
+
+
+        Of the real rooms from the list above, the sum of their sector IDs is 1514.
+*/
+
+
+    [TestClass]
+    public class Day4Tests
+    {
+        #region extract
+        [TestMethod]
+        public void Checksum1ExtractedCorrectly()
+        {
+            string checksum = "";
+            string input = "aaaaa-bbb-z-y-x-123[abxyz]";
+
+            var decoder = new RoomDecoder();
+
+            checksum = decoder.ExtractChecksum(input);
+
+            Assert.AreEqual("abxyz", checksum);
+
+        }
+
+        [TestMethod]
+        public void Checksum2ExtractedCorrectly()
+        {
+            string checksum = "";
+            string input = "a-b-c-d-e-f-g-h-987[abcde]";
+
+            var decoder = new RoomDecoder();
+
+            checksum = decoder.ExtractChecksum(input);
+
+            Assert.AreEqual("abcde", checksum);
+
+        }
+
+        [TestMethod]
+        public void Checksum3ExtractedCorrectly()
+        {
+            string checksum = "";
+            string input = "not-a-real-room-404[oarel]";
+
+            var decoder = new RoomDecoder();
+
+            checksum = decoder.ExtractChecksum(input);
+
+            Assert.AreEqual("oarel", checksum);
+
+        }
+
+        [TestMethod]
+        public void Checksum4ExtractedCorrectly()
+        {
+            string checksum = "";
+            string input = "totally-real-room-200[decoy]";
+
+            var decoder = new RoomDecoder();
+
+            checksum = decoder.ExtractChecksum(input);
+
+            Assert.AreEqual("decoy", checksum);
+
+        }
+        #endregion
+
+        #region Calculate
+        [TestMethod]
+        public void Checksum1CalculatedCorrectly()
+        {
+            string checksum = "";
+            string input = "aaaaa-bbb-z-y-x-123[abxyz]";
+
+            var decoder = new RoomDecoder();
+
+            checksum = decoder.CalculateChecksum(input);
+
+            Assert.AreEqual("abxyz", checksum);
+
+        }
+
+        [TestMethod]
+        public void Checksum2CalculatedCorrectly()
+        {
+            string checksum = "";
+            string input = "a-b-c-d-e-f-g-h-987[abcde]";
+
+            var decoder = new RoomDecoder();
+
+            checksum = decoder.CalculateChecksum(input);
+
+            Assert.AreEqual("abcde", checksum);
+
+        }
+
+        [TestMethod]
+        public void Checksum3CalculatedCorrectly()
+        {
+            string checksum = "";
+            string input = "not-a-real-room-404[oarel]";
+
+            var decoder = new RoomDecoder();
+
+            checksum = decoder.CalculateChecksum(input);
+
+            Assert.AreEqual("oarel", checksum);
+
+        }
+
+        [TestMethod]
+        public void Checksum4CalculatedCorrectly()
+        {
+            string checksum = "";
+            string input = "totally-real-room-200[decoy]";
+
+            var decoder = new RoomDecoder();
+
+            checksum = decoder.CalculateChecksum(input);
+
+            Assert.AreEqual("loart", checksum);
+
+        }
+
+#endregion
+
+        [TestMethod]
+        public void Checksum1Validate()
+        {
+            bool checksumIsValid = false;
+            string input = "aaaaa-bbb-z-y-x-123[abxyz]";
+
+            var decoder = new RoomDecoder();
+
+            checksumIsValid = decoder.ValidateChecksum(input);
+
+            Assert.IsTrue(checksumIsValid);
+        }
+
+        [TestMethod]
+        public void Checksum4Validate()
+        {
+            bool checksumIsValid = false;
+            string input = "totally-real-room-200[decoy]";
+
+            var decoder = new RoomDecoder();
+
+            checksumIsValid = decoder.ValidateChecksum(input);
+
+            Assert.IsFalse(checksumIsValid);
+        }
+
+        [TestMethod]
+        public void GetSortCode1()
+        {
+            int sortCode = -1;
+            string input = "aaaaa-bbb-z-y-x-123[abxyz]";
+
+            var decoder = new RoomDecoder();
+
+            sortCode = decoder.GetSortCode(input);
+
+            Assert.AreEqual(123, sortCode);
+        }
+
+        [TestMethod]
+        public void GetAnswerPart1()
+        {
+            IEnumerable<string> list = System.IO.File.ReadAllLines("../../../ChecksumList.txt");
+            var decoder = new RoomDecoder();
+            var sortCodeSum = 0;
+
+            foreach (var input in list)
+            {
+                if(decoder.ValidateChecksum(input))
+                {
+                    sortCodeSum += decoder.GetSortCode(input);
+                }
+            }
+
+            Assert.AreEqual(158835, sortCodeSum);
+        }
+
+        [TestMethod]
+        public void DecryptString()
+        {
+            /*To decrypt a room name, rotate each letter forward through the alphabet a number of times equal to the room's sector ID. 
+             * A becomes B, B becomes C, Z becomes A, and so on. Dashes become spaces.
+
+            For example, the real name for qzmt - zixmtkozy - ivhz - 343 is very encrypted name.*/
+
+            var input = "qzmt-zixmtkozy-ivhz-343";
+            var decryptResult = "";
+            var decoder = new RoomDecoder();
+
+            decryptResult = decoder.DecryptString(input);
+
+            Assert.AreEqual("very encrypted name", decryptResult);
+        }
+
+        [TestMethod]
+        public void GetAnswer2()
+        {
+            IEnumerable<string> list = System.IO.File.ReadAllLines("../../../ChecksumList.txt");
+            var decoder = new RoomDecoder();
+            var rightString = "north pole objects";
+            var rightSortCode = -2;
+
+            foreach(var input in list)
+            {
+                var decryptResult = decoder.DecryptString(input);
+
+                if(decryptResult.Contains("north"))
+                {
+                    rightSortCode = decoder.GetSortCode(input);
+                }
+
+                if (String.Equals(rightString, decryptResult, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    
+                    break;
+                }
+            }
+
+            Assert.AreEqual(993, rightSortCode);
+        }
+    }
+}
---
+++ Advent2016/RoomDecoder.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Advent2016
+{
+    public class RoomDecoder
+    {
+        private Regex checksumExtractRegex = new Regex(@"(?<=\[)[^\[]+?(?=\])");
+        private Regex checksumCalculateRegex = new Regex(@"^.*?(?=\d)");
+        private Regex checksumSortCode = new Regex(@"\d(\d+?)\d");
+        private List<char> alphabet = "abcdefghijklmnopqrstuvwxyz".ToList();
+
+        public string ExtractChecksum(string input)
+        {
+            //String format: 
+            string checksum = "";
+
+            checksum = checksumExtractRegex.Match(input).Value;
+
+            return checksum;
+        }
+
+        public string CalculateChecksum(string input)
+        {
+            string checksum = "";
+
+            var rawString = checksumCalculateRegex.Match(input).Value;
+
+            rawString = rawString.Replace("-", "");
+
+            //Count each char
+
+            checksum = GetTopFiveFromString(rawString);
+
+            return checksum;
+        }
+
+        private static string GetTopFiveFromString(string rawString)
+        {
+            var charFrequencies = new Dictionary<char, int>();
+
+            foreach (var character in rawString)
+            {
+                if (charFrequencies.ContainsKey(character))
+                {
+                    charFrequencies[character]++;
+                }
+                else
+                {
+                    charFrequencies[character] = 1;
+                }
+            }
+
+            var top5 = charFrequencies.ToList()
+                                      .OrderByDescending(entry => entry.Value)
+                                      .ThenBy(entry => entry.Key)
+                                      .Take(5)
+                                      .Select(entry => entry.Key);
+
+            return String.Join("", top5);
+        }
+
+        public bool ValidateChecksum(string input)
+        {
+            var extractedChecksum = this.ExtractChecksum(input);
+
+            var calculatedChecksum = this.CalculateChecksum(input);
+
+            return (extractedChecksum == calculatedChecksum);
+        }
+
+        public int GetSortCode(string input)
+        {
+            int sortCode = -1;
+
+            var sortCodeString = checksumSortCode.Match(input).Value;
+
+            int.TryParse(sortCodeString, out sortCode);
+
+            return sortCode;
+        }
+
+        public string DecryptString(string input)
+        {
+            var decryptedString = "";
+
+            //Get the string to decrypt
+            var rawString = checksumCalculateRegex.Match(input).Value;
+
+            var sortCode = this.GetSortCode(input);
+
+            foreach (var character in rawString)
+            {
+                if (character == '-')
+                {
+                    decryptedString += " ";
+                }
+                else
+                {
+                    int currentLetter = alphabet.IndexOf(character);
+
+                    int decryptedIndex = (currentLetter + sortCode) % 26;
+
+                    decryptedString += alphabet[decryptedIndex];
+                }
+            }
+
+            return decryptedString.Trim();
+        }
+    }
+}
